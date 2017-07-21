2df04515376390b4aef9b42d28cc5c5f875b108a
Fernando de Francisco <fernando@nextail.co>
2017-07-21 20:31:44 +0200

    Added io server side and static serving

--- server/index.js
+++ server/index.js
@@ -5,9 +5,15 @@ var io = require('socket.io')(server);
 
 var port = 6677;
 
+app.use(express.static('client'));
+
 app.get('/hola-mundo', function(req, res) {
     res.status(200).send('Hola mundo desde una ruta');
 });
+
+io.on('connection', function(socket) {
+    console.log("El nodo " + socket.handshake.address + " se ha conectado");
+})
 
 server.listen(port, function() {
     console.log('Servidor está funcionando el puerto ' + port);
